9e865cbfdd8d1421e68aca3023fb6d2a57f49912
Michael Belousov <michael.belousov98@gmail.com>
2020-02-13 21:02:06 -0500

    add more complicated test statements for basic parser testing

--- sizr-format/src/main.rs
+++ sizr-format/src/main.rs
@@ -7,14 +7,48 @@ use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "format.pest"]
-pub struct LParser;
+pub struct FormatDescParser;
+
+use std::fs;
+use std::collections::HashMap;
+use std::io::{self, Read};
 
 fn main() {
-    let test_src = r"
-    my_node: '''
-        $here \ $is?some $possible:node \?body
-    '''
-    ";
-    let parsed = LParser::parse(Rule::node_body, test_src);
-    println!("{:?}", parsed);
+
+  let hellovar = FormatDescParser::parse(Rule::var, "$hello")
+      .expect("unsuccessful parse")
+      .next()
+      .unwrap();
+  println!("VAR: {:#?}", hellovar);
+
+  let nodebody = FormatDescParser::parse(Rule::node_body, "'''$hello'''")
+      .expect("unsuccessful parse")
+      .next()
+      .unwrap();
+  println!("NODEBODY: {:#?}", nodebody);
+
+  let nodedecl = FormatDescParser::parse(Rule::node_decl, "my_rule: '''$hello'''")
+      .expect("unsuccessful parse")
+      .next()
+      .unwrap();
+  println!("NODE_DECL: {:#?}", nodedecl);
+
+  /*
+  let src_file =
+    fs::read_to_string("example.sizf")
+        .expect("cannot read file");
+  let file = FormatDescParser::parse(Rule::file, &src_file)
+      .expect("unsuccessful parse")
+      .next()
+      .unwrap();
+  println!("{:#?}", file);
+  */
+
+  let mut buffer = String::new();
+  io::stdin().read_to_string(&mut buffer);
+  let input = FormatDescParser::parse(Rule::file, &buffer)
+      .expect("unsuccessful parse")
+      .next()
+      .unwrap();
+  println!("STDIN: {:#?}", input);
 }
