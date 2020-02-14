4bba653e7de2dd7496d21f502630d3a534a4fd6b
Michael Belousov <michael.belousov98@gmail.com>
2020-02-13 20:01:21 -0500

    fixed basic grammar, need to add tests

--- sizr-format/src/main.rs
+++ sizr-format/src/main.rs
@@ -10,8 +10,8 @@ use pest::Parser;
 pub struct LParser;
 
 fn main() {
-    let parsed = LParser::parse(Rule::field, "-273.15");
+    let parsed = LParser::parse(Rule::node_body, "-273.15");
     println!("{:?}", parsed);
-    let failed = LParser::parse(Rule::field, "not a number!");
+    let failed = LParser::parse(Rule::node_body, "not a number!");
     println!("{:?}", failed);
 }
