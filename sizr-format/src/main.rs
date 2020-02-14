b35b359b055306aa015e1e006ae94bd36bfb6a0b
Michael Belousov <michael.belousov98@gmail.com>
2020-02-13 19:52:00 -0500

    added some basic parser tutorial and extended grammar stuff to start

--- sizr-format/src/main.rs
+++ sizr-format/src/main.rs
@@ -1,3 +1,17 @@
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "format.pest"]
+pub struct LParser;
+
 fn main() {
-    println!("Hello, world!");
+    let parsed = LParser::parse(Rule::field, "-273.15");
+    println!("{:?}", parsed);
+    let failed = LParser::parse(Rule::field, "not a number!");
+    println!("{:?}", failed);
 }
