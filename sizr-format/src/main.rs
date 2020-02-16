826e677e12dda5635891b76791d92dff623941f5
Michael Belousov <michael.belousov98@gmail.com>
2020-02-16 09:24:00 -0500

    developing more logic and layouts

--- sizr-format/src/main.rs
+++ sizr-format/src/main.rs
@@ -11,28 +11,137 @@ pub struct FormatDescParser;
 
 use std::fs;
 use std::collections::HashMap;
+use std::vector::Vec;
 use std::io::{self, Read};
 use std::env;
 
-// I have no idea how rust does this stuff yet, I'm just spitballing here
-trait Write {
-}
-trait VariableWrite {
-}
-trait StringWrite {
-}
-trait ConditionalWrite {
-}
-trait BreakWrite {
+pub enum Expr {
+        // Comparison
+        LessThan            {l: Box<Expr>, r: Box<Expr>},
+        GreaterThan         {l: Box<Expr>, r: Box<Expr>},
+        Equal               {l: Box<Expr>, r: Box<Expr>},
+        NotEqual            {l: Box<Expr>, r: Box<Expr>},
+        GreaterThanOrEqual  {l: Box<Expr>, r: Box<Expr>},
+        // Arithmetic
+        LessThanOrEqual     {l: Box<Expr>, r: Box<Expr>},
+        Add                 {l: Box<Expr>, r: Box<Expr>},
+        Sub                 {l: Box<Expr>, r: Box<Expr>},
+        Mult                {l: Box<Expr>, r: Box<Expr>},
+        Pow                 {l: Box<Expr>, r: Box<Expr>},
+        Divide              {l: Box<Expr>, r: Box<Expr>},
+        Remainder           {l: Box<Expr>, r: Box<Expr>},
+        IntDivide           {l: Box<Expr>, r: Box<Expr>},
+        Negate              {l: Box<Expr>, r: Box<Expr>},
+        // Logical
+        Or                  {l: Box<Expr>, r: Box<Expr>},
+        And                 {l: Box<Expr>, r: Box<Expr>},
+        Xor                 {l: Box<Expr>, r: Box<Expr>},
+        Complement          {e: Box<Expr>},
+        // Special
+        Parenthesized       {e: Box<Expr>}
+    }
+
+pub enum WriteCommand {
+    Literal(String),
+    Breakpoint,
+    Cond { expr: Expr
+         , if_: Box<WriteCommand>
+         , else_: Box<WriteCommand>
+         }
 }
 
-struct NodeContext {
+//make serializable for caching
+struct NodeFormat{
+    // TODO: use inkwell to JIT the format rule
+    writeCommands: Vec<WriteCommand>,
 }
 
-//serializable for cached binary
-struct NodeFormat {
-    // switch to using inkwell to JIT the format rule
+struct Node {
+    type_: str,
 }
+
+static mut node_formats: HashMap<String, &NodeFormat> = HashMap::with_capacity(100);
+
+enum Value {
+    Number(f64),
+    String(std::string::String),
+    Bool_(bool)
+}
+
+type Context = HashMap<String, Value>;
+
+// built in fluster style cast operator?
+// with the types: f, float, i, int, n: number, s: string, b:
+// e.g.: `class $name $props.len:b
+// no that looks ugly, should implicitly convert a decent amount...
+// depends totally on idioms, but at least no need to convert to boo,
+// should coerce python style
+// maybe use rational numbers instead of number=floats|integer?
+//
+// Type coercion rules:
+
+fn exec_bin_op(expr: &Expr, l: &Value, r: &Value) {
+    match (l, r) {
+        number, number => true
+        string, string => true
+        bool_, bool_ => true
+        _, _ => panic!()
+    }
+    match expr {
+        EExpr::LessThan {l, r} => l < r,
+        EExpr::GreaterThan => l > r,
+        EExpr::Equal => l == r,
+        EExpr::NotEqual => l != r,
+        EExpr::GreaterThanOrEqual => l >= r,
+        EExpr::LessThanOrEqual => l <= r,
+        EExpr::Add => l + r,
+        EExpr::Sub => l - r,
+        EExpr::Mult => l * r,
+        EExpr::Pow => l.pow(r),
+        EExpr::Divide => l / r,
+        EExpr::IntDivide => l / r,
+        EExpr::Remainder => l % r,
+        EExpr::Negate => -l,
+        EExpr::Or => l | r,
+        EExpr::And => l & r,
+        EExpr::Xor => l ^ r,
+        EExpr::Complement{e} => ~e,
+    }
+}
+
+fn serialize(node: &Node, ctx: &Context) {
+    let format = node_formats[node.type_];
+    for cmd in format.writeCommands {
+        match cmd {
+            WriteCommand::Literal(s) => ctx.writes.last().append(s)
+            WriteCommand::Breakpoint => ctx.writes.append(String::from(""))
+            WriteCommand::Expr(e) => match e {
+                EExpr::LessThan{l,r} => l < r,
+                EExpr::GreaterThan{l,r} => l > r,
+                EExpr::Equal{l,r} => l == r,
+                EExpr::NotEqual{l,r} => l != r,
+                EExpr::GreaterThanOrEqual{l,r} => l >= r,
+                EExpr::LessThanOrEqual{l,r} => l <= r,
+                EExpr::Add{l,r} => l + r,
+                EExpr::Sub{l,r} => l - r,
+                EExpr::Mult{l,r} => l * r,
+                EExpr::Pow{l,r} => l.pow(r),
+                EExpr::Divide{l,r} => l / r,
+                EExpr::IntDivide{l,r} => l / r,
+                EExpr::Remainder{l,r} => l % r,
+                EExpr::Negate{l,r} => -l,
+                EExpr::Or{l,r} => l | r,
+                EExpr::And{l,r} => l & r,
+                EExpr::Xor{l,r} => l ^ r,
+                EExpr::Complement{e} => ~e,
+            }
+
+        }
+    }
+}
+
+// TODO: write custom parser for performance reasons, because of the weird way
+// characters will be dealt with
 
 fn main() {
 
