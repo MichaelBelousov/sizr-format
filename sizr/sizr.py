29fc08ac5d0931f361d53502fb19c0700190ffbb
Michael Belousov <mike.belousov@bentley.com>
2020-09-11 20:37:54 -0400

    good enough python prototype for sizr transforms parser, will start engine

--- sizr/sizr.py
+++ sizr/sizr.py
@@ -32,9 +32,10 @@ class ScopeProp: # TODO: make these namespaces, or mypy dict interfaces?
 capture_any = re.compile('.*')
 
 class CaptureExpr:
-    def __init__(self, matcher = capture_any):
-        self.matcher = matcher
-    __repr__ = __str__ = lambda s: f'<Capture|{s.matcher}>'
+    def __init__(self, pattern = capture_any, name = None):
+        self.pattern = pattern
+        self.name = name
+    __repr__ = __str__ = lambda s: f'<Capture|name={s.name},pattern={s.pattern}>'
 
 class ScopeExpr:
     def __init__(self, nesting_op: str = None):
@@ -59,66 +60,74 @@ class IsCaptureErr(Exception): pass
 class IsNestingOpErr(Exception): pass
 class IsEoiErr(Exception): pass
 class IsTransformOpErr(Exception): pass
+class RejectedParseErr(Exception): pass
 
-def skipLeadingSpace(parse_ctx: ParseCtx):
-    while parse_ctx.loc < len(parse_ctx.src) and parse_ctx.src[parse_ctx.loc].isspace():
-        parse_ctx.loc += 1
+def skipLeadingSpace(ctx: ParseCtx):
+    while ctx.loc < len(ctx.src) and ctx.src[ctx.loc].isspace():
+        ctx.loc += 1
 
 # TODO: start using this over .find and preprocess
-def nextWhiteSpaceOffset(ctx: ParseCtx) -> int:
+def nextTokenEndOffset(ctx: ParseCtx) -> int:
     whitespace_pattern = re.compile(r' \t\n')
-    return whitespace_pattern.search(ctx.remaining_src).start()
+    space_match = whitespace_pattern.search(ctx.remaining_src)
+    return space_match.start() if space_match is not None else len(ctx.remaining_src)
 
-def parseValue(parse_ctx: ParseCtx):
-    remaining_src = parse_ctx.src[parse_ctx.loc:]
+def parseValue(ctx: ParseCtx):
+    remaining_src = ctx.src[ctx.loc:]
     is_quote = remaining_src[0] == '"'
     is_num = remaining_src[0].isdigit()
     if is_quote:
         unescaped_quote_pattern = re.compile(r'(?<!\\)"')
         end_quote_offset = unescaped_quote_pattern.search(remaining_src[1:]).end() + 1
-        parse_ctx.loc += end_quote_offset
-        skipLeadingSpace(parse_ctx)
-        # NOTE: when switching to parse_ctx.remaining_src this won't work anymore
+        ctx.loc += end_quote_offset
+        skipLeadingSpace(ctx)
+        # NOTE: when switching to ctx.remaining_src this won't work anymore
         return remaining_src[1:end_quote_offset-1]
     elif is_num:
         leading_num_pattern = re.compile(r'^[1-9]\d*(\.\d+)?')
         number_end = leading_num_pattern.search(remaining_src).end()
         number_sentence = remaining_src[:number_end]
-        parse_ctx.loc += number_end
-        skipLeadingSpace(parse_ctx)
+        ctx.loc += number_end
+        skipLeadingSpace(ctx)
         return float(number_sentence)
     else:
         next_space_offset = remaining_src.find(' ')
-        parse_ctx.loc += next_space_offset
-        skipLeadingSpace(parse_ctx)
-        # NOTE: when switching to parse_ctx.remaining_src this won't work anymore
+        ctx.loc += next_space_offset
+        skipLeadingSpace(ctx)
+        # NOTE: when switching to ctx.remaining_src this won't work anymore
         return remaining_src[:next_space_offset]
 
-def parseCapture(parse_ctx: ParseCtx):
-    remaining_src = parse_ctx.src[parse_ctx.loc:]
+def isCapture(ctx: ParseCtx) -> bool:
+    return bool(ctx.remaining_src) and ctx.remaining_src[0] == '$'
+
+def parseCapture(ctx: ParseCtx):
+    if not ctx.remaining_src: raise RejectedParseErr()
+    remaining_src = ctx.src[ctx.loc:]
     is_regex_capture = remaining_src[:2] == '$/'
     is_named_capture = remaining_src[1:2].isalpha()
+    is_anonymous_capture = remaining_src[0] == '$'
     if is_regex_capture:
         unescaped_slash_pattern = re.compile(r'(?<!\\)/')
         end_slash_offset = unescaped_slash_pattern.search(remaining_src[2:]).end() + 2
         regex_src = remaining_src[2:end_slash_offset]
-        parse_ctx.loc += end_slash_offset
-        skipLeadingSpace(parse_ctx)
+        ctx.loc += end_slash_offset
+        skipLeadingSpace(ctx)
         return CaptureExpr(re.compile(regex_src))
     elif is_named_capture:
         next_space_offset = remaining_src.find(' ')
         name = remaining_src[1:next_space_offset]
-        parse_ctx.loc += next_space_offset
-        skipLeadingSpace(parse_ctx)
+        ctx.loc += next_space_offset
+        skipLeadingSpace(ctx)
         # TODO: escape regex metachars and/or disallow some chars
-        return CaptureExpr(re.compile(name))
-    else:
-        parse_ctx.loc += 1
-        skipLeadingSpace(parse_ctx)
+        return CaptureExpr(capture_any, name)
+    elif is_anonymous_capture:
+        ctx.loc += 1
+        skipLeadingSpace(ctx)
         return CaptureExpr()
+    else: raise RejectedParse()
 
-def isNestingOp(parse_ctx: ParseCtx) -> bool:
-    remaining_src = parse_ctx.src[parse_ctx.loc:]
+def isNestingOp(ctx: ParseCtx) -> bool:
+    remaining_src = ctx.src[ctx.loc:]
     # none of these intersect right now
     ops = ['.', '(', '<', '{', '[', ';', ',', '#', '@', ':']
     return any(map(lambda op: remaining_src.startswith(op), ops))
@@ -132,67 +141,72 @@ def parseNestingOp(ctx: ParseCtx) -> bool:
     skipLeadingSpace(ctx)
     return op
 
-# TODO: follow python iterator convention,
-# let parse_ctx have like an 'iter_parse_scope_props' method to do this repeatedly
-def parseScopeProp(parse_ctx: ParseCtx) -> ScopeProp:
-    # TODO: remove alias
-    remaining_src = parse_ctx.remaining_src
-    if not parse_ctx.remaining_src: raise IsEoiErr()
-    # TODO: move out to reusable `at_capture` matcher
-    is_capture = remaining_src[0] == '$'
-    if is_capture: raise IsCaptureErr()
-    if isTransformOp(parse_ctx): raise IsTransformOpErr()
-    if isNestingOp(parse_ctx): raise IsNestingOpErr()
-    next_space_index = remaining_src.find(' ') # TODO: change to findNextSpaceOffset
-    if (next_space_index == -1): raise IsEoiErr()
-    if_boolean_sentence = remaining_src[:next_space_index]
-    is_boolean_no_prop = remaining_src[0] == '!'
+def parseIdentifier(ctx: ParseCtx) -> str:
+    if not ctx.remaining_src: raise RejectedParseErr("expected an identifier")
+    identifier_pattern = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*')
+    match = identifier_pattern.search(ctx.remaining_src)
+    identifier = ctx.remaining_src[match.start():match.end()]
+    ctx.loc += match.end()
+    skipLeadingSpace(ctx)
+    return identifier
+
+# TODO: follow python iterator convention
+# i.e. let ctx have like an 'iter_parse_scope_props' method to do this repeatedly
+def parseScopeProp(ctx: ParseCtx) -> ScopeProp:
+    token_end = nextTokenEndOffset(ctx)
+    if_boolean_sentence = ctx.remaining_src[:token_end]
+    is_boolean_no_prop = ctx.remaining_src[0] == '!'
     if is_boolean_no_prop:
-        parse_ctx.loc += len(if_boolean_sentence)
-        skipLeadingSpace(parse_ctx)
+        ctx.loc += len(if_boolean_sentence)
+        skipLeadingSpace(ctx)
         return ScopeProp(if_boolean_sentence[1:], False)
     is_specific_value_prop = '=' in if_boolean_sentence
     is_boolean_yes_prop = not is_specific_value_prop
     if is_boolean_yes_prop:
-        parse_ctx.loc += len(if_boolean_sentence)
-        skipLeadingSpace(parse_ctx)
+        ctx.loc += len(if_boolean_sentence)
+        skipLeadingSpace(ctx)
         return ScopeProp(if_boolean_sentence)
     elif is_specific_value_prop:
-        value_delim_index = remaining_src.find('=')
-        key = remaining_src[:value_delim_index]
-        parse_ctx.loc += value_delim_index + 1
-        skipLeadingSpace(parse_ctx)
-        value = parseValue(parse_ctx)
+        value_delim_index = ctx.remaining_src.find('=')
+        key = ctx.remaining_src[:value_delim_index]
+        ctx.loc += value_delim_index + 1
+        skipLeadingSpace(ctx)
+        value = parseValue(ctx)
         return ScopeProp(key, value)
 
-def parseScopeExpr(parse_ctx: ParseCtx) -> ScopeExpr:
+def isScopeProp(ctx: ParseCtx) -> bool:
+    if not ctx.remaining_src: return False
+    firstchar = ctx.remaining_src[0]
+    return firstchar == '!' or firstchar.isalpha()
+
+def parseScopeExpr(ctx: ParseCtx) -> ScopeExpr:
     expr = ScopeExpr()
-    had_explicit_capture = False # FIXME: will always have explicit capture...
-    try:
-        while True:
-            prop = parseScopeProp(parse_ctx)
-            print(prop)
-            expr.properties.append(prop)
-    except IsCaptureErr:
-        had_explicit_capture = True
-        expr.capture = parseCapture(parse_ctx)
-    except IsNestingOpErr:
-        expr.nesting_op = parseNestingOp(parse_ctx)
-    if had_explicit_capture:
-        try:
-            expr.nesting_op = parseNestingOp(parse_ctx)
-        except IsTransformOpErr: pass
-        except IsEoiErr: pass
+    while isScopeProp(ctx):
+        prop = parseScopeProp(ctx)
+        expr.properties.append(prop)
+    if isCapture(ctx):
+        expr.capture = parseCapture(ctx)
+    else:
+        # FIXME: confusing logic
+        last_prop = expr.properties.pop()
+        identifier = last_prop.key
+        expr.capture = CaptureExpr(re.compile(identifier))
+    if isNestingOp(ctx):
+        expr.nesting_op = parseNestingOp(ctx)
     return expr
+
+def isQuery(ctx: ParseCtx) -> bool:
+    return isScopeProp(ctx) or isCapture(ctx)
+
+isScopeExpr = isQuery
 
 def parseQuery(ctx: ParseCtx) -> Query:
     query = Query()
-    try:
-        while True:
-            expr = parseScopeExpr(ctx)
-            print(expr)
-            query.nested_scopes.append(expr)
-    except IsTransformOpErr: pass
+    # while not isTransformOp(ctx):
+    while isScopeExpr(ctx):
+        expr = parseScopeExpr(ctx)
+        print(expr)
+        query.nested_scopes.append(expr)
     return query
 
 def isTransformOp(ctx: ParseCtx):
@@ -208,13 +222,12 @@ def parseTransformOp(ctx: ParseCtx):
 def parseTransform(src: str) -> Transform:
     ctx = ParseCtx(src)
     selector = assertion = destructive = None
-    try:
+    if isQuery(ctx):
         selector = parseQuery(ctx)
-    except IsTransformOpErr: pass
-    destructive = parseTransformOp(ctx) == ">>!"
-    try:
+    if isTransformOp(ctx): # assert isTransformOp
+        destructive = parseTransformOp(ctx) == ">>!"
+    if isQuery(ctx):
         assertion = parseQuery(ctx)
-    except IsEoiErr: pass
     assert selector or assertion, "a transform command must have either a selector or an assertion"
     return Transform(selector, assertion, destructive)
 
@@ -227,5 +240,4 @@ def main():
     except KeyboardInterrupt:
         print()
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': main()
