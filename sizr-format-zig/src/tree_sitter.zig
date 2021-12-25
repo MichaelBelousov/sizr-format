914ccf2cac28f7af9d64dc22b84ef6336fae501f
Michael Belousov <mike.belousov@bentley.com>
2021-12-25 17:52:21 -0500

    more untested binding work

--- sizr-format-zig/src/tree_sitter.zig
+++ sizr-format-zig/src/tree_sitter.zig
@@ -2,6 +2,7 @@ const c_api = @cImport({
     @cInclude("tree_sitter/api.h");
 });
 
+// TODO: doing this by hand for now but some kind of guided clang plugin could probably generate bindings better
 const TsParser = struct {
     _ts_parser: *c_api.TSParser,
 
@@ -18,6 +19,18 @@ const TsParser = struct {
     fn free(self: Self) void {
         c_api.ts_parser_delete(self._ts_parser);
     }
+
+    fn set_language(self: Self, language: TSLanguage) void {
+        c_api.ts_parser_set_language(self._ts_parser, language.c);
+    }
+};
+
+const TsNode = struct {
+    _ts_node: *c_api.TSNode,
+};
+
+const TsLanguage = struct {
+    _c: *c_api.TSLanguage,
 };
 
 test "TsParser" {
