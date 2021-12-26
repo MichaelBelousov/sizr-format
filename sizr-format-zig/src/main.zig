2b947547fb1496cca1411adea401c613795bb951
Michael Belousov <mike.belousov@bentley.com>
2021-12-26 15:22:07 -0500

    free all alloced resources in test

--- sizr-format-zig/src/main.zig
+++ sizr-format-zig/src/main.zig
@@ -22,8 +22,10 @@ test "parse" {
     if (!parser.set_language(ts.cpp()))
         @panic("couldn't set cpp lang");
     const tree = parser.parse_string(null, src);
+    defer ts._c.ts_tree_delete(tree._c);
     const root = ts._c.ts_tree_root_node(tree._c);
     const syntax_tree = ts._c.ts_node_string(root);
+    defer std.c.free(syntax_tree);
     std.debug.print("syntax_tree: '{s}'\n", .{syntax_tree});
 }
 
