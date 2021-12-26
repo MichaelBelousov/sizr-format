11be44105ce652c8fda4891b719fe2d4f97ff6c7
Michael Belousov <mike.belousov@bentley.com>
2021-12-26 08:54:46 -0500

    added tree-sitter-cpp dep and working on basic tree-sitter bindings

--- sizr-format-zig/src/tree_sitter.zig
+++ sizr-format-zig/src/tree_sitter.zig
@@ -4,33 +4,31 @@ const c_api = @cImport({
 
 // TODO: doing this by hand for now but some kind of guided clang plugin could probably generate bindings better
 const TsParser = struct {
-    _ts_parser: *c_api.TSParser,
+    _c: *c_api.TSParser,
 
     const Self = @This();
 
     fn init() Self {
         const maybe_c_parser = c_api.ts_parser_new();
         if (maybe_c_parser) |c_parser| {
-            return Self{ ._ts_parser = c_parser };
+            return Self{ ._c = c_parser };
         } else unreachable;
     }
 
     // TODO: check zig idioms
-    fn free(self: Self) void {
-        c_api.ts_parser_delete(self._ts_parser);
+    pub fn free(self: Self) void {
+        c_api.ts_parser_delete(self._c);
     }
 
-    fn set_language(self: Self, language: TSLanguage) void {
-        c_api.ts_parser_set_language(self._ts_parser, language.c);
+    pub fn set_language(self: Self, language: TsLanguage) void {
+        return c_api.ts_parser_set_language(self._c, language._c);
     }
-};
 
-const TsNode = struct {
-    _ts_node: *c_api.TSNode,
-};
-
-const TsLanguage = struct {
-    _c: *c_api.TSLanguage,
+    // TODO: steal the documentation from the C header
+    // would be nice if this could be done with reflection
+    pub fn parse_string(self: Self, old_tree: ?TsTree, src_string: []const u8) TsTree {
+        return TsTree{ ._c = c_api.ts_parser_parse_string(self._c, if (old_tree) |old_tree_val| old_tree_val._c else null, src_string, src_string.len) };
+    }
 };
 
 test "TsParser" {
@@ -39,3 +37,17 @@ test "TsParser" {
         parser.free();
     }
 }
+
+const TsNode = struct {
+    _c: *c_api.TSNode,
+};
+
+const TsTree = struct {
+    _c: *c_api.TSTree,
+};
+
+const TsLanguage = struct {
+    _c: *c_api.TSLanguage,
+};
+
+test "TsLanguage" {}
