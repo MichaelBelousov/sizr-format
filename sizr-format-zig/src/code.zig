a7b67dbdbea20631a3d1322797255600806c260a
Michael Belousov <mike.belousov@bentley.com>
2021-12-28 23:56:19 -0500

    finally remove the polymorphism crap

--- sizr-format-zig/src/code.zig
+++ sizr-format-zig/src/code.zig
@@ -10,47 +10,22 @@ const Value = union(enum) {
     node,
 };
 
-const Resolver = struct {
-    const Self = @This();
-    resolveFn: fn (self: Self, expr: Expr) ?Value,
-    pub fn resolve(self: Self, expr: Expr) ?Value {
-        return self.resolveFn(self, expr);
-    }
-};
+// works if I make it return not optional
+fn f(expr: Expr) ?Value {
+    var prng = std.rand.DefaultPrng.init(0);
 
-const LangResolver = struct {
-    const Self = @This();
-
-    resolver: Resolver,
-
-    fn resolveFn(resolver: Resolver, expr: Expr) ?Value {
-        const self = @fieldParentPtr(Self, "resolver", &resolver);
-        _ = self;
-
-        var prng = std.rand.DefaultPrng.init(0);
-
-        return switch(expr) {
-            .name => |name|
-                if (std.mem.eql(u8, name, "type"))
-                    Value{ .string = "test" }
-                else if (prng.random().int(u32) == 0)
-                    Value{ .node = {} }
-                else
-                    Value{ .node = {} },
-            else => null
-        };
-    }
-
-    pub fn init() Self {
-        return Self{
-            .resolver = Resolver{
-                .resolveFn = Self.resolveFn
-            },
-        };
-    }
-};
+    return switch(expr) {
+        .name => |name|
+            if (std.mem.eql(u8, name, "type"))
+                Value{ .string = "test" }
+            else if (prng.random().int(u32) == 0)
+                Value{ .node = {} }
+            else
+                Value{ .node = {} },
+        else => null
+    };
+}
 
 test "write" {
-    const langr = LangResolver.init();
-    _ = langr;
+    _ = f(Expr{.name = "test"});
 }
