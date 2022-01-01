36a4a5eef2289e989d1754abef51257a20e92b60
Michael Belousov <mike.belousov@bentley.com>
2022-01-01 18:12:42 -0500

    remove all unions

--- sizr-format-zig/README.sh
+++ sizr-format-zig/README.sh
@@ -1,4 +1,13 @@
+# I know so far that switch/else-if triggers this issue
+# I tried converting it to if/else-if and it didn't work
+
 ~/personal/zig/build/bin/zig test src/code.zig \
   --strip -O ReleaseFast --verbose-llvm-ir -fsingle-threaded -fno-unwind-tables -fno-compiler-rt -mno-red-zone -fno-PIE -fno-sanitize-c -dynamic \
   2>&1 \
   # > out.llvm.ir
+
+# how to increment a mass amount of llvm ir instructions
+
+# getclip \
+# | python3 -c 'import sys,re;print(re.sub(r"%(\d+)", lambda m: f"%{y+1 if (y:=int(m[1])) > 30 else y}", sys.stdin.read()))'
+# | putclip
--- sizr-format-zig/src/code.zig
+++ sizr-format-zig/src/code.zig
@@ -1,28 +1,19 @@
-const Expr = union(enum) {
-    a: u32,
-    b,
-};
-
-const Value = union(enum) {
-    c: u32,
-    d,
-};
-
 // works if I make it return not optional
-fn f(expr: Expr) ?Value {
+fn f(use: bool, param: u32) ?u32 {
     var val: u32 = 0;
-    return switch(expr) {
-        .a => |a|
-            if (a == 0)
-                Value{ .c = 0}
-            else if (val == 0)
-                Value{ .d = {} }
-            else
-                Value{ .d = {} },
-        else => Value{.d = {}}
+    //var 
+    return switch (use) {
+        true => (
+        if (param == 0)
+            @as(u32, 0)
+        else if (val == 0)
+            @as(u32, 1)
+        else
+            @as(u32, 2)
+    ), else => @as(u32, 3)
     };
 }
 
 test "" {
-    _ = f(Expr{.a = 0});
+    _ = f(true, 0);
 }
