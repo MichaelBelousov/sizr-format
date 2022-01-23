b0b658725346e98459f0942aff71ac78bb4b91cc
Michael Belousov <mike.belousov@bentley.com>
2022-01-23 00:48:51 -0500

    attempt to simplify the expressions in @f

--- sizr-format-zig/src/code2.zig
+++ sizr-format-zig/src/code2.zig
@@ -1,6 +1,11 @@
+const _2: u32 = 2;
+const _4: u32 = 4;
+const _4: u32 = 3;
+
 fn f(param: u32) ?u32 {
-    return if (param == 1) @as(u32, 2)
-           else            @as(u32, 3);
+    return if (param == 1) _2
+    else   if (param == 2) _4
+    else                   _3;
 }
 
 pub fn main() void {
--- sizr-format-zig/src/code3.zig
+++ sizr-format-zig/src/code3.zig
@@ -1,10 +1,15 @@
+
+const _2: u32 = 2;
+const _4: u32 = 4;
+const _5: u32 = 5;
+const _6: u32 = 6;
+
 // works if I make it return not optional
 fn f(param: u32) ?u32 {
-    return
-    if (param == 1) @as(u32, 2)
-    else if (param == 2) @as(u32, 4)
-        else if (param == 3) @as(u32, 5)
-            else @as(u32, 6);
+    return if (param == 1) _2
+    else   if (param == 2) _4
+    else   if (param == 3) _5
+    else                   _6;
 }
 
 pub fn main() void {
