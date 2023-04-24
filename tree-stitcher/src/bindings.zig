0739168d1c09207dc4f19bd9a6675f6c48fac710
Michael Belousov <mike.belousov@bentley.com>
2023-04-24 13:25:16 -0400

    use mmap

--- tree-stitcher/src/bindings.zig
+++ tree-stitcher/src/bindings.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 const ts = @import("tree-sitter");
+const mman = @cImport({
+    @cInclude("sys/mman.h");
+});
 
 export fn my_test(x: i32) [*]const u8 {
     _ = x;
@@ -20,26 +23,28 @@ export fn exec_query(
     _ = srcs_count;
     defer file.close();
 
-    var src: [8192]u8 = undefined;
-    const bytes_read = file.readAll(&src) catch {
-        std.debug.print("openFileZ failed", .{});
+    var file_len = (file.stat() catch {
+        std.debug.print("stat file failed", .{});
         return null;
-    };
-    // TODO: mmap
-    // var src = std.c.mmap(path);
+    }).size;
 
-    if (bytes_read >= src.len) {
-        std.log.err("File was too long", .{});
-        return null;
+    var src_ptr = @alignCast(
+        std.mem.page_size,
+        std.c.mmap(null, file_len, mman.PROT_READ, mman.MAP_FILE, file.handle, 0)
+    );
+    var src = @ptrCast([*]const u8, src_ptr)[0..file_len];
+    defer {
+        var result = std.c.getErrno(std.c.munmap(src_ptr, file_len));
+        if (result != .SUCCESS)
+            std.debug.print("munmap errno: {any}", .{ result });
     }
-    src[src.len - 1] = '\x00';
 
     const parser = ts.Parser.new();
     defer parser.free();
     if (!parser.set_language(ts.cpp()))
         @panic("couldn't set cpp lang");
 
-    const tree = parser.parse_string(null, &src);
+    const tree = parser.parse_string(null, src);
     defer tree.delete();
     const root = tree.root_node();
     const syntax_tree_str = root.string();
@@ -59,7 +64,7 @@ export fn exec_query(
             const capture_str = capture_node.string();
             defer capture_str.free();
             std.debug.print("capture: {s}\n", .{capture_str.ptr});
-            std.debug.print("capture source: {s}\n", .{capture_node.in_source(&src)});
+            std.debug.print("capture source: {s}\n", .{capture_node.in_source(src)});
         }
         return &match;
     } else {
