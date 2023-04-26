957869ed63cf955a43901e6b9b8e0bc926aeb476
Michael Belousov <mike.belousov@bentley.com>
2023-04-26 10:59:30 -0400

    fix chibi ffi bindings

--- tree-stitcher/tree-sitter-chibi-ffi.scm
+++ tree-stitcher/tree-sitter-chibi-ffi.scm
@@ -5,21 +5,18 @@
 (define-c-struct tstree)
 
 (define-c-struct tsnode
-  ((array unsigned-long 4) c_field_name "context")
-  ((const (pointer void)) c_field_name "id")
-  ((const (pointer tstree)) c_field_name "tree"))
+  ((array unsigned-long 4) context context)
+  ((const (pointer void)) id id)
+  ((const (pointer tstree)) tree tree))
 
+(define-c-struct query_capture
+  (tsnode node node)
+  (unsigned-long index index))
 
-; (define-c-struct query_capture
-  ; (tsnode c_field_name "node")
-  ; (unsigned-long c_field_name "index"))
+(define-c-struct query_match
+  [finalizer: free_query_match]
+  (int c_field_name capture_count capture-count)
+  ((pointer string) captures captures))
 
-
-; (define-c-struct query_match
-  ; [finalizer: free_query_match]
-  ; (int c_field_name "capture_count")
-  ; ((pointer string) c_field_name "captures"))
-
-; array result generates bad code...
-; (define-c (free (array (const query_match) null)) exec_query ((const string) (array (const string))))
+(define-c (free (array (const query_match) null)) exec_query ((const string) (array (const string))))
 
