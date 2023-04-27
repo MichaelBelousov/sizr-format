505c1c5e11c0170ce865361d2a9229ca671b4751
Michael Belousov <mike.belousov@bentley.com>
2023-04-27 14:16:16 -0400

    macro and expr->string for better lisp binding

--- tree-stitcher/tests/query.scm
+++ tree-stitcher/tests/query.scm
@@ -1,9 +1,26 @@
+;#!r7rs
+(import (chibi io)
+        (scheme small))
+
 (load "zig-out/lib/libbindings.so")
+
+(define (expr->string e)
+  (call-with-port
+    (open-output-string)
+    (lambda (out)
+      (write e out)
+      (get-output-string out))))
+
+(define-syntax exec_query2
+  (syntax-rules ()
+    ((exec_query2 exp path)
+     (matches_ExecQueryResult (exec_query (expr->string (quote exp)) '(path))))))
+
 (define q (exec_query "((function_definition) @func)" '("/home/mike/test2.cpp")))
-; (display (captures (match (car q))))
-(display (matches_ExecQueryResult q))
-(display "\n")
-(display (ts_node_string (node (captures (car (matches_ExecQueryResult q))))))
-(display "\n")
-(display (node_source (node (captures (car (matches_ExecQueryResult q)))) q))
-(display "\n")
+
+; (display (ts_node_string (node (captures (car (matches_ExecQueryResult q))))))
+; (display "\n")
+; (display (node_source (node (captures (car (matches_ExecQueryResult q)))) q))
+; (display "\n")
+
+(display (exec_query2 ((function_definition) @func) "/home/mike/test2.cpp"))
