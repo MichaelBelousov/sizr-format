ed7f0e4b19e04d0a2468d7d8b03843b8dc50e29d
Michael Belousov <mike.belousov@bentley.com>
2023-05-20 20:04:14 -0400

    simplify (or not) debug-dlopen.c

--- tree-stitcher/debug-dlopen.sh
+++ tree-stitcher/debug-dlopen.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/bash
 
-cat debug-dlopen.c | clang -x c - && ./a.out zig-out/lib/libbindings.so
+clang -x c - <<EOF && ./a.out zig-out/lib/libbindings.so
+#include <stdio.h>
+#include <dlfcn.h>
+#include <assert.h>
+
+int main(int argc, const char** argv) {
+  assert(argc >= 2);
+  void* handle = dlopen(argv[1], RTLD_NOW);
+  printf("arg: %s\n", argv[1]);
+  printf("handle: %p\n", handle);
+  printf("error: %s\n", dlerror());
+}
+EOF
+
 rm a.out
 
