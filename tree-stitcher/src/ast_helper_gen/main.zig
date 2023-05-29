06c8469ab5948a03b9c41e396595968c8e20c3a5
Michael Belousov <mike.belousov@bentley.com>
2023-05-29 17:48:57 -0400

    reformat ast-helper-gen with 4 spaces

--- tree-stitcher/src/ast_helper_gen/main.zig
+++ tree-stitcher/src/ast_helper_gen/main.zig
@@ -3,29 +3,29 @@ const json = std.json;
 const clap = @import("zig-clap");
 
 const sexp = union (enum) {
-  str: []const u8,
-  symbol: []const u8,
-  pair: struct {
-    car: *sexp,
-    cdr: *sexp,
-  },
+    str: []const u8,
+    symbol: []const u8,
+    pair: struct {
+        car: *sexp,
+        cdr: *sexp,
+    },
 
-  pub fn write(self: @This(), writer: *std.io.Writer) !void {
-    switch (self) {
-      .str => |data| writer.write(data),
-      .symbol => |data| {
-        writer.write("'");
-        writer.write(data);
-      },
-      .pair => |data| {
-        writer.write("(");
-        data.car.write(writer);
-        writer.write(" ");
-        data.cdr.write(writer);
-        writer.write(")");
-      },
+    pub fn write(self: @This(), writer: *std.io.Writer) !void {
+        switch (self) {
+            .str => |data| writer.write(data),
+            .symbol => |data| {
+                writer.write("'");
+                writer.write(data);
+            },
+            .pair => |data| {
+                writer.write("(");
+                data.car.write(writer);
+                writer.write(" ");
+                data.cdr.write(writer);
+                writer.write(")");
+            },
+        }
     }
-  }
 };
 
 pub fn main() !void {
