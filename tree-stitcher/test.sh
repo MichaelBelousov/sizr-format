cbb3baa2c97ac6f6e0bce5b8e54827e62e34b11a
Michael Belousov <michael.belousov98@gmail.com>
2023-06-02 00:23:18 -0400

    add comment for future test refactor

--- tree-stitcher/test.sh
+++ tree-stitcher/test.sh
@@ -5,4 +5,5 @@ script_dir="$(dirname $0)"
 # tests
 find $script_dir/tests -name '*.test.scm' | sed -e 's,^,(load ",' -e 's,$,"),' \
   | chibi-scheme
+# TODO: I think I can just pass $(find ...) to chibi-scheme
 
